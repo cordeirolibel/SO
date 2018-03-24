5834dc2e1b9b087a61a362d536af8ccaa0684901
cordeirolibel <gustavolibel@gmail.com>
2018-03-24 17:19:26 -0300

    sublime, makefile and queue.c

---
+++ p00/queue.c
@@ -0,0 +1,44 @@
+/**========================================================
+| UTFPR  2018   Cperating System
+| https://github.com/gabrielsouzaesilva/
+| https://github.com/cordeirolibel/
+=========================================================**/
+
+#include "queue.h"
+
+//------------------------------------------------------------------------------
+// Insere um elemento no final da fila.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - o elemento deve existir
+// - o elemento nao deve estar em outra fila
+
+void queue_append (queue_t **queue, queue_t *elem) ;
+
+//------------------------------------------------------------------------------
+// Remove o elemento indicado da fila, sem o destruir.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - a fila nao deve estar vazia
+// - o elemento deve existir
+// - o elemento deve pertencer a fila indicada
+// Retorno: apontador para o elemento removido, ou NULL se erro
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem) ;
+
+//------------------------------------------------------------------------------
+// Conta o numero de elementos na fila
+// Retorno: numero de elementos na fila
+
+int queue_size (queue_t *queue) ;
+
+//------------------------------------------------------------------------------
+// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
+// elemento é feita por uma função externa, definida pelo programa que
+// usa a biblioteca. Essa função deve ter o seguinte protótipo:
+//
+// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) ) ;
+
+#endif
